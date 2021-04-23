8a5f8b3302581c7ee5e2fae6070a426c99ccb21e
zembahk <77575523+zembahk@users.noreply.github.com>
2021-04-22 19:01:00 -0700

    Update HogeCollector.py

--- HogeCollector.py
+++ HogeCollector.py
@@ -3,7 +3,10 @@ import random
 import arcade
 import os
 
-SPRITE_SCALING = 0.5
+
+SPRITE_AVATAR_SCALING = 0.125
+SPRITE_COIN_SCALING = 0.25
+
 
 SCREEN_WIDTH = 1024
 SCREEN_HEIGHT = 600
@@ -41,17 +44,17 @@ class MyGame(arcade.Window):
         arcade.set_background_color(arcade.color.AMAZON)
 
         # zembahk edit
-        self.total_time = 0.0
-
+        self.total_time = -3.0
+        self.win_time = 0.0
+        self.win_txt = "You Won!"
+        self.restart_wait = 1.0
 
     def setup(self):
         """ Set the value of the variable """
-        # zembahk edit
-        self.total_time = 0.0
+
+        self.total_time = -3.0
         self.win_time = 0.0
-
-
-        
+       
         # Assign values ​​to background variables
         self.background = arcade.load_texture("images/background.jpg")
 
@@ -61,7 +64,7 @@ class MyGame(arcade.Window):
 
         # Set player character
         self.score = 0
-        self.player_sprite = arcade.Sprite("images/character.png", SPRITE_SCALING / 4)
+        self.player_sprite = arcade.Sprite("images/character.png", SPRITE_AVATAR_SCALING)
         self.player_sprite.center_x = 50
         self.player_sprite.center_y = 50
         self.player_list.append(self.player_sprite)
@@ -69,14 +72,30 @@ class MyGame(arcade.Window):
         for i in range(100):
 
             # Instantiate gold coins
-            coin = arcade.Sprite("images/hoge_coin.png", SPRITE_SCALING / 2)
+            coin = arcade.Sprite("images/hoge_coin.png", SPRITE_COIN_SCALING)
 
             # Place gold coins
-            coin.center_x = random.randrange(SCREEN_WIDTH)
-            coin.center_y = random.randrange(SCREEN_HEIGHT)
+            coin.center_x = random.randrange((coin.width + 150), SCREEN_WIDTH - (coin.width + 150))
+            coin.center_y = random.randrange((coin.height + 150), SCREEN_HEIGHT - (coin.height + 150))
 
+            # Give direction
+            coin_slowest = 30
+            coin_fastest = 350
+            while True:
+                coin.delta_x = random.randrange(coin_fastest * -1, coin_fastest)
+                coin.delta_y = random.randrange(coin_fastest * -1, coin_fastest)
+                if (coin.delta_x > coin_slowest or coin.delta_x < coin_slowest * -1) and (coin.delta_y > coin_slowest or coin.delta_y < coin_slowest * -1):
+                    break
+                
+            # Set up the initial angle, and the "spin"
+            coin.angle = random.randrange(360)
+            while True:
+                coin.change_angle = random.randrange(-2, 7)
+                if coin.change_angle > 1 or coin.change_angle < -0.5:
+                    break
             # Add gold coins to the list
             self.coin_list.append(coin)
+            
 
     def on_draw(self):
         """
@@ -87,41 +106,64 @@ class MyGame(arcade.Window):
         arcade.start_render()
 
         # Draw a textured rectangle 
-        arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2,
-                                      SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
+        arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2, SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
 
-        # Draw all characters
-        self.coin_list.draw()
-        self.player_list.draw()
+        # Draw title box in the middle of the screen.
+        if self.total_time < 0:
+            output = "HOGE COLLECTOR"
+            arcade.draw_text(output, SCREEN_WIDTH // 2, SCREEN_HEIGHT // 1.3, arcade.color.WHITE, 36, width=400, align="center", anchor_x="center", anchor_y="center")
 
-        # Draw text 
-        arcade.draw_text(f"Score: {self.score}", 10, 20, arcade.color.WHITE, 14)
+            if self.total_time < -2:
+                arcade.draw_text("3", SCREEN_WIDTH // 2, SCREEN_HEIGHT // 1.5, arcade.color.WHITE, 54, width=75, align="center", anchor_x="center", anchor_y="center")
+            if self.total_time > -2 and self.total_time < -1:
+                arcade.draw_text("2", SCREEN_WIDTH // 2, SCREEN_HEIGHT // 1.5, arcade.color.WHITE, 54, width=75, align="center", anchor_x="center", anchor_y="center")
+            if self.total_time > -1 and self.total_time < 0:
+                arcade.draw_text("1", SCREEN_WIDTH // 2, SCREEN_HEIGHT // 1.5, arcade.color.WHITE, 54, width=75, align="center", anchor_x="center", anchor_y="center")
 
-
-        # Calculate seconds by using a modulus (remainder)
-        seconds = int(self.total_time) % 60
-
-        # Figure out our output
-        output = f"Time: {seconds:02d}"
-        # zemabhk edit
-        winTime = f"Won in {self.win_time:.6f} seconds"
-        
-        # Output the timer text.
-        # zembahk edit
-        if self.win_time == 0:
-            arcade.draw_text(output, 10, 50, arcade.color.WHITE, 18)
         else:
-            arcade.draw_text(winTime, 10, 100, arcade.color.WHITE, 26)
+            
+
+            # Draw all characters
+            self.coin_list.draw()
+            self.player_list.draw()
+
+            # Draw text 
+            arcade.draw_text(f"Score: {self.score}", 10, 20, arcade.color.WHITE, 14)
+
+
+            # Calculate seconds by using a modulus (remainder)
+            seconds = float(self.total_time) % 60
+
+            # Figure out our output
+            output = f"Time: {seconds:.2f}"
+            # zemabhk edit
+            self.win_txt = f"Won in {self.win_time:.6f} seconds"
+        
+            # Output the timer text.
+            # zembahk edit
+            if self.win_time == 0:
+                arcade.draw_text(output, 10, 50, arcade.color.WHITE, 18)
+            elif self.win_time != 0 and self.total_time - self.win_time > self.restart_wait:
+                arcade.draw_text(f"{self.win_time:.6f}", SCREEN_WIDTH // 2, SCREEN_HEIGHT // 1.3, arcade.color.WHITE, 64, align="center", anchor_x="center", anchor_y="center")
+                arcade.draw_text("Press Space To Restart", SCREEN_WIDTH // 2, SCREEN_HEIGHT // 5, arcade.color.WHITE, 26, align="center", anchor_x="center", anchor_y="center")
+            else:
+                arcade.draw_text(self.win_txt, 10, 50, arcade.color.WHITE, 26)
+
+
+    def on_key_press(self, key, key_modifiers):
+        """
+        Called whenever a key on the keyboard is pressed.
+        """
+        if self.win_time != 0 and key == 32 and self.total_time - self.win_time > self.restart_wait:
+                self.setup()
             
     def on_update(self, delta_time):
         """
         All the logic to move, and the game logic goes here.
         """
-        if self.win_time == 0:
-            self.total_time += delta_time
-        else:
-            self.total_time = self.win_time
 
+        self.total_time += delta_time
+             
     def on_mouse_motion(self, x, y, dx, dy):
         """
         Mouse movement event
@@ -134,6 +176,19 @@ class MyGame(arcade.Window):
 
         # Gold coin list update
         self.coin_list.update()
+        coin_left = len(self.coin_list)
+        for i in range(0, coin_left):
+            coin = self.coin_list[i]
+            
+            # Figure out if we hit the edge and need to reverse.
+            wall_buffer = 5
+            if coin.center_x < coin.width // 2 + wall_buffer or coin.center_x > SCREEN_WIDTH - coin.width // 2 - wall_buffer:
+                coin.delta_x *= -1
+            if coin.center_y < coin.height // 2 + wall_buffer or coin.center_y > SCREEN_HEIGHT - coin.height // 2 - wall_buffer:
+                coin.delta_y *= -1
+
+            coin.center_x += coin.delta_x * delta_time
+            coin.center_y += coin.delta_y * delta_time
 
         # Collision detection between player and gold coin
         hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.coin_list)
@@ -146,15 +201,24 @@ class MyGame(arcade.Window):
         # zembahk edit
         if self.score == 100 and self.win_time == 0: 
             self.win_time = self.total_time
-
             
 
-def main():
+def play():
     """ Main method """
+    
     window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
     window.setup()
     arcade.run()
 
 
 if __name__ == "__main__":
-    main()
+    #while True:
+        play()
+        #print(MyGame.win_time)
+        #answer = input("Restart?(y/n)  ")
+        #if answer == 'y':
+            
+           # continue
+       # else:
+          #  break
+    
